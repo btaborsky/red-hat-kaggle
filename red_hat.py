3a3ae040cc2bcf76ee9643689e2cb1d5a5eabb2c
Ben Taborsky <btaborsky@gmail.com>
2016-08-31 14:03:59 -0700

    feat: merge people and events, add weekday feature, create a couple simple plots

--- red_hat.py
+++ red_hat.py
@@ -1,3 +1,7 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import pdb
 
@@ -10,9 +14,34 @@ def load_data():
 	event_fname = "Data/act_train.csv"
 
 
-	people_df = pd.read_csv(people_fname)
+	people_df = pd.read_csv(people_fname,parse_dates=["date"])
 
-	events_df = pd.read_csv(event_fname)
+	new_name_dict = {"people_id":"people_id"}
+	for col in people_df.columns:
+		if col not in new_name_dict:
+			new_name_dict[col] = "p_"+col
 
-	pdb.set_trace()
-	return people_df,events_df
+	people_df = people_df.rename(columns=new_name_dict)
+	
+	events_df = pd.read_csv(event_fname,parse_dates=["date"])
+	events_df = pd.merge(events_df,people_df,on="people_id")
+	events_df["weekday"] = events_df["date"].apply(pd.tslib.Timestamp.weekday)
+
+
+	
+	return events_df
+
+def visualizations(events_df):
+
+	value_per_day = events_df[["date","outcome"]].groupby("date").mean()
+
+	plt.plot(value_per_day.index,value_per_day["outcome"])
+	plt.show()
+
+	outcome_per_weekday = events_df[["weekday","outcome"]].groupby("weekday").mean()
+	plt.bar(outcome_per_weekday.index,outcome_per_weekday["outcome"])
+	plt.show()
+
+
+
+
